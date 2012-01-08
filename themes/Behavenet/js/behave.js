c3943c230c520de9385febda2b9b6ae0eba30dbc
Mike Keran <info@mikekeran.com>
2012-01-07 17:51:10 -0800

    Added Directories listing and JS-tree functionality

--- themes/Behavenet/js/behave.js
+++ themes/Behavenet/js/behave.js
@@ -5,4 +5,44 @@ if (Drupal.jsEnabled) {
       return false;
     });
   });
+
+  Drupal.behaviors.behavenet = function() {
+    // options
+    var o = {
+      open_char : '&#9660;',
+      close_char : '&#9658;',
+      animation : 300
+    };
+
+    $('ul.bef-tree').each(function() {
+      // Find any nested UL elements and hide them until exposed.
+      $(this).find('ul').each(function() {
+        $(this)
+          .hide()
+          .parent().children('div.form-item')
+            .prepend('<span class="jtree close" style="cursor:pointer;">' + o.close_char + '</span>')
+        ;
+      });
+    });
+
+    // Setup click events
+    $('span.jtree').click(function() {
+      if ($(this).hasClass('close')) {
+        $(this)
+          .html(o.open_char)
+          .removeClass('close')
+          .addClass('open')
+          .parent().parent().children('ul').show(o.animation)
+        ;
+      }
+      else if ($(this).hasClass('open')) {
+        $(this)
+          .html(o.close_char)
+          .removeClass('open')
+          .addClass('close')
+          .parent().parent().children('ul').hide(o.animation)
+        ;
+      }
+    });
+  };
 }
