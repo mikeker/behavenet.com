82d26ae51dbd5871c3855424003d3b6408cabe95
Mike Keran <info@mikekeran.com>
2012-02-14 11:48:38 -0800

    Cleaned up search results

---
+++ themes/Behavenet/templates/views-view--search--page.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file views-view--search--page.tpl.php
+ * View template for the search results page.
+ *
+ * Since we're borrowing Views' backend for search and rewriting the results
+ * outselves, we don't print the main content area.
+ *
+ * @see behavenet_views_pre_render in behave.module
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <?php if ($admin_links): ?>
+    <div class="views-admin-links views-hide">
+      <?php print $admin_links; ?>
+    </div>
+  <?php endif; ?>
+  <?php if ($header): ?>
+    <div class="view-header">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($exposed): ?>
+    <div class="view-filters">
+      <?php print $exposed; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($attachment_before): ?>
+    <div class="attachment attachment-before">
+      <?php print $attachment_before; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($rows && $pager): ?>
+    <?php print $pager; ?>
+  <?php endif; ?>
+
+  <?php if ($attachment_after): ?>
+    <div class="attachment attachment-after">
+      <?php print $attachment_after; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($more): ?>
+    <?php print $more; ?>
+  <?php endif; ?>
+
+  <?php if ($footer): ?>
+    <div class="view-footer">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($feed_icon): ?>
+    <div class="feed-icon">
+      <?php print $feed_icon; ?>
+    </div>
+  <?php endif; ?>
+
+</div> <?php /* class view */ ?>
