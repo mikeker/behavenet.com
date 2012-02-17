be13c69b719eca1917b3d326a72a82392e028b92
Mike Keran <info@mikekeran.com>
2012-02-17 11:36:47 -0800

    Added collapsible menu code and styling

--- themes/Behavenet/js/behave.js
+++ themes/Behavenet/js/behave.js
@@ -1,12 +1,26 @@
 if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    $('select.jump-menu').change(function() {
+
+  Drupal.behaviors.behavenet = function() {
+    behave_jump_menu_init($('select.jump-menu:not(processed)'));
+    behave_collasible_menu_init($('ul.bef-tree:not(.processed)'), true);
+    behave_collasible_menu_init($('.behavenet-collapsible-menu ul:not(processed)'), false);
+  };
+  
+  /*
+   * Helper functions
+   */
+  
+  /**
+   * Initialize jump menus that are not built by Views.
+   */
+  function behave_jump_menu_init($elem) {
+    $elem.change(function() {
       window.location = $(this).val();
       return false;
     });
-  });
-
-  Drupal.behaviors.behavenet = function() {
+  }
+  
+  function behave_collasible_menu_init($elem, isBEF) {
     // options
     var o = {
       open_char : '&#9660;',
@@ -14,37 +28,74 @@ if (Drupal.jsEnabled) {
       animation : 300
     };
 
-    $('ul.bef-tree:not(.processed), .behavenet-collapsible-menu ul:not(processed)').each(function() {
+    $elem.each(function() {
       // Find any nested UL elements and hide them until exposed.
-      $(this)
-        .addClass('processed')
-        .find('ul').each(function() {
-          $(this)
-            .hide()
-            .parent().children('div.form-item')
-              .prepend('<span class="jtree close" style="cursor:pointer;">' + o.close_char + '</span>')
-          ;
-        });
+      if (isBEF) {
+        $(this)
+          .addClass('processed')
+          .find('ul').each(function() {
+            $(this)
+              .hide()
+              .parent().children('div.form-item')
+                .prepend('<span class="jtree close" style="cursor:pointer;">' + o.close_char + '</span>')
+                .addClass('hasChildren')
+            ;
+          });
+      }
+      else {
+        $(this)
+          .addClass('processed')
+          .find('ul').each(function() {
+            $(this)
+              .hide()
+              .parent()
+                .prepend('<span class="jtree close" style="cursor:pointer;">' + o.close_char + '</span>')
+                .addClass('hasChildren')
+            ;
+          });
+      }
     });
 
     // Setup click events
-    $('span.jtree').click(function() {
-      if ($(this).hasClass('close')) {
-        $(this)
-          .html(o.open_char)
-          .removeClass('close')
-          .addClass('open')
-          .parent().parent().children('ul').show(o.animation)
-        ;
-      }
-      else if ($(this).hasClass('open')) {
-        $(this)
-          .html(o.close_char)
-          .removeClass('open')
-          .addClass('close')
-          .parent().parent().children('ul').hide(o.animation)
-        ;
-      }
-    });
-  };
+    if (isBEF) {
+      $('span.jtree').click(function() {
+        if ($(this).hasClass('close')) {
+          $(this)
+            .html(o.open_char)
+            .removeClass('close')
+            .addClass('open')
+            .parent().parent().children('ul').show(o.animation)
+          ;
+        }
+        else if ($(this).hasClass('open')) {
+          $(this)
+            .html(o.close_char)
+            .removeClass('open')
+            .addClass('close')
+            .parent().parent().children('ul').hide(o.animation)
+          ;
+        }
+      });
+    }
+    else {
+      $('span.jtree').click(function() {
+        if ($(this).hasClass('close')) {
+          $(this)
+            .html(o.open_char)
+            .removeClass('close')
+            .addClass('open')
+            .siblings('ul').show(o.animation)
+          ;
+        }
+        else if ($(this).hasClass('open')) {
+          $(this)
+            .html(o.close_char)
+            .removeClass('open')
+            .addClass('close')
+            .siblings('ul').hide(o.animation)
+          ;
+        }
+      });
+    }
+  }  
 }
