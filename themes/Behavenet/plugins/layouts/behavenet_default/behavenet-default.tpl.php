ebf9137baa64711b736cfb5670799da4fdb48382
Mike Keran <info@mikekeran.com>
2012-04-27 16:21:10 -0700

    Changed right-hand callout to generic, fixed-width sidebar

--- themes/Behavenet/plugins/layouts/behavenet_default/behavenet-default.tpl.php
+++ themes/Behavenet/plugins/layouts/behavenet_default/behavenet-default.tpl.php
@@ -1,35 +1,28 @@
-<?php
+  <?php
 /*
  * This pane is intented to be used as the main page content pane,
  * thus the overall #page wrapper and the page-x-y-z div IDs.
  */
 ?>
 <div id="page">
-  <div id="page-main-wrapper">
-    <?php if (!empty($content['header'])): ?>
-      <div class="panel-pane" id="page-header">
-        <?php print $content['header']; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($content['main_callout'])): ?>
-      <div class="panel-pane" id="page-main-callout">
-        <?php print $content['main_callout']; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($content['main'])): ?>
-      <div class="panel-pane" id="page-main">
-        <?php print $content['main']; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($content['footer'])): ?>
-      <div class="panel-pane" id="page-footer">
-        <?php print $content['footer']; ?>
-      </div>
-    <?php endif; ?>
+  <div class="panel-pane" id="page-header">
+    <?php if (!empty($content['header'])) { print $content['header']; } ?>
   </div>
-  <div class="panel-pane" id="page-left-sidebar">
-    <?php if (!empty($content['left_sidebar'])): ?>
-      <?php print $content['left_sidebar']; ?>
-    <?php endif; ?>
+  
+  <div id="content-wrapper">
+    <div class="panel-pane" id="page-main">
+      <?php if (!empty($content['main'])) { print $content['main']; } ?>
+    </div>
+    <div class="panel-pane" id="page-left-sidebar">
+      <?php if (!empty($content['left_sidebar'])) { print $content['left_sidebar']; } ?>
+    </div>
+    <div class="panel-pane" id="page-main-callout"><div id="page-main-callout-inner">
+      <?php if (!empty($content['main_callout'])) { print $content['main_callout']; } ?>
+    </div></div>
   </div>
+  
+  <div class="panel-pane" id="page-footer">
+    <?php if (!empty($content['footer'])) { print $content['footer']; } ?>
+  </div>
+  
 </div>
