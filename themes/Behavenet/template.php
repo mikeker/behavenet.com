9292f9475b43e3259bb988fc7f9d265058cc0258
Mike Keran <info@mikekeran.com>
2012-10-04 14:13:07 -0700

    Ooops, left in debug code

--- themes/Behavenet/template.php
+++ themes/Behavenet/template.php
@@ -127,7 +127,6 @@ function behavenet_preprocess_content_field(&$vars) {
         continue;
       }
       $combo = node_load($item['nid']);
-      dsm($combo); dsm($item);
       $title = array();
       foreach ($combo->field_combo_drugs as $info) {
         $generic = node_load($info['nid']);
