f553d6f5c7c6a1c9298f86202b6d46d3b63b5b5d
Mike Keran <info@mikekeran.com>
2013-04-05 15:22:00 -0700

    Initial responsive design layout.

--- themes/Behavenet/plugins/layouts/behavenet_default/behavenet-default.tpl.php
+++ themes/Behavenet/plugins/layouts/behavenet_default/behavenet-default.tpl.php
@@ -8,21 +8,21 @@
   <div class="panel-pane" id="page-header">
     <?php if (!empty($content['header'])) { print $content['header']; } ?>
   </div>
-  
+
   <div id="content-wrapper">
     <div class="panel-pane" id="page-main">
       <?php if (!empty($content['main'])) { print $content['main']; } ?>
     </div>
-    <div class="panel-pane" id="page-left-sidebar">
-      <?php if (!empty($content['left_sidebar'])) { print $content['left_sidebar']; } ?>
-    </div>
     <div class="panel-pane" id="page-main-callout"><div id="page-main-callout-inner">
       <?php if (!empty($content['main_callout'])) { print $content['main_callout']; } ?>
     </div></div>
+    <div class="panel-pane" id="page-left-sidebar">
+      <?php if (!empty($content['left_sidebar'])) { print $content['left_sidebar']; } ?>
+    </div>
   </div>
-  
+
   <div class="panel-pane" id="page-footer">
     <?php if (!empty($content['footer'])) { print $content['footer']; } ?>
   </div>
-  
+
 </div>
